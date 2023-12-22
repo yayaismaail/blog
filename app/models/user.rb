7f459d4b5e2d403a30d1bd137e3ba53fc97bb5ed
ismail halliru muhammad <ismailhm888@gmail.com>
2023-12-22 15:17:57 +0100

    make the comments and posts counters work as expected

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,8 @@
 # Top-level documentation comment for User module
 class User < ApplicationRecord
-  has_many :posts, foreign_key: 'author_id'
-  has_many :comments, foreign_key: 'author_id'
-  has_many :likes, foreign_key: 'author_id'
+  has_many :posts, foreign_key: 'author_id', dependent: :destroy
+  has_many :comments, foreign_key: 'author_id', dependent: :destroy
+  has_many :likes, foreign_key: 'author_id', dependent: :destroy
 
   def recent_posts
     posts.order(created_at: :desc).limit(3)
